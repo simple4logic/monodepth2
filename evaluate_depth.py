bf1d242b5ee457d0e4cefb8a41e226cff067cca0
hyun <okht12@gmail.com>
2024-08-26 18:25:42 +0900

    use polar instead KITTIRAW

--- evaluate_depth.py
+++ evaluate_depth.py
@@ -80,7 +80,10 @@ def evaluate(opt):
 
         encoder_dict = torch.load(encoder_path)
 
-        dataset = datasets.KITTIRAWDataset(opt.data_path, filenames,
+        # dataset = datasets.KITTIRAWDataset(opt.data_path, filenames,
+        #                                    encoder_dict['height'], encoder_dict['width'],
+        #                                    [0], 4, is_train=False)
+        dataset = datasets.polarDataset(opt.data_path, filenames,
                                            encoder_dict['height'], encoder_dict['width'],
                                            [0], 4, is_train=False)
         dataloader = DataLoader(dataset, 16, shuffle=False, num_workers=opt.num_workers,
