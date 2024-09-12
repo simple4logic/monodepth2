853b7825bd89f74a2a5f5c64e0f142acd860ee4f
Hyuntae H. <okht12@gmail.com>
2024-09-13 02:02:05 +0900

    models option added

--- test_simple.py
+++ test_simple.py
@@ -41,7 +41,9 @@ def parse_args():
                             "mono+stereo_no_pt_640x192",
                             "mono_1024x320",
                             "stereo_1024x320",
-                            "mono+stereo_1024x320"])
+                            "mono+stereo_1024x320",
+                            "my_trained_mono",
+                            "last_trained_mono"])
     parser.add_argument('--ext', type=str,
                         help='image extension to search for in folder', default="jpg")
     parser.add_argument("--no_cuda",
